e3d1fd1e365b5237d4a8dcc308d96d6c9528070c
aouyang1 <aouyang1@gmail.com>
2016-04-14 18:46:55 -0700

    add_digits solution and tests from ao

---
+++ add_digits/add_digit_ao.go
@@ -0,0 +1,31 @@
+package main_ao
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func addDigits(num int) int {
+
+	numstr := strconv.Itoa(num)
+
+	if len(numstr) == 1 {
+		return num
+	} else {
+		var running_sum int
+
+		for _, val := range numstr {
+			digit, _ := strconv.Atoi(string(val))
+			running_sum += digit
+		}
+		return addDigits(running_sum)
+	}
+
+}
+
+func main() {
+	a := 12345
+	b := addDigits(a)
+
+	fmt.Println(b)
+}
---
+++ add_digits/add_digit_test.go
@@ -0,0 +1,43 @@
+package main_ao
+
+import (
+	"testing"
+)
+
+func TestAddSingleDigit(t *testing.T) {
+	if result := addDigits(0); result != 0 {
+		t.Errorf("Expected result of 0, but got %d instead.", result)
+	}
+
+	if result := addDigits(9); result != 9 {
+		t.Errorf("Expected result of 9, but got %d instead.", result)
+	}
+}
+
+func TestAddDoubleDigit(t *testing.T) {
+	if result := addDigits(10); result != 1 {
+		t.Errorf("Expected result of 1, but got %d instead.", result)
+	}
+
+	if result := addDigits(14); result != 5 {
+		t.Errorf("Expected result of 5, but got %d instead.", result)
+	}
+
+	if result := addDigits(19); result != 1 {
+		t.Errorf("Expected result of 1, but got %d instead.", result)
+	}
+}
+
+func TestAddTripleDigit(t *testing.T) {
+	if result := addDigits(100); result != 1 {
+		t.Errorf("Expected result of 1, but got %d instead.", result)
+	}
+
+	if result := addDigits(123); result != 6 {
+		t.Errorf("Expected result of 6, but got %d instead.", result)
+	}
+
+	if result := addDigits(987); result != 6 {
+		t.Errorf("Expected result of 6, but got %d instead.", result)
+	}
+}
